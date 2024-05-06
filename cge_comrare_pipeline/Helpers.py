a21c2d99a3817cb06dfa75e684acd7ec40624e21
luisggon <luisggon@flyde.io>
2024-05-06 10:45:34 +0200

    added new argument

--- cge_comrare_pipeline/Helpers.py
+++ cge_comrare_pipeline/Helpers.py
@@ -39,7 +39,10 @@ def arg_parser()->dict:
     parser.add_argument('--steps', type=str, nargs='?', default=None, const=None, help='Full path to the JSON file containing the pipeline steps to be executed.')
 
     # order of the pipeline
-    parser.add_argument('--pca-first', type=str, nargs='?', default=None, const=None, help='boolean that determines if the pca analysis is done before or after the sample qc.')
+    parser.add_argument('--pca-first', type=str, nargs='?', default=False, const=None, help='boolean that determines if the pca analysis is done before or after the sample qc.')
+
+    # execute kingship analysis instead of ibd
+    parser.add_argument('--use-kingship', type=str, nargs='?', default=False, const=None, help='boolean that determines if the relatives and duplicates prune is run using kingship or IBD.')
 
     # parse args and turn into dict
     args = parser.parse_args()
