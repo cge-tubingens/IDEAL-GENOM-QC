71731967012fda368b7bde7083165e6f87e89bc3
Luis Giraldo <luisggon@protonmail.com>
2025-11-18 16:04:40 +0100

    shell_do function has been dropped and superseeded by a better one

--- ideal_genom/utilities/Helpers.py
+++ ideal_genom/utilities/Helpers.py
@@ -2,32 +2,6 @@ import subprocess
 import sys
 import argparse
 import os
-
-
-def shell_do(command, print_cmd=False, log=False, return_log=False, err=False):
-    """
-    From GenoTools
-    """
-    
-    if print_cmd:
-        print(f'Executing: {(" ").join(command.split())}', file=sys.stderr)
-
-    res = subprocess.run(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    # Check if the command failed
-    if res.returncode != 0:
-        error_message = f"Command failed with return code {res.returncode}: {res.stderr.decode('utf-8')}"
-        print(error_message, file=sys.stderr)
-        raise RuntimeError(error_message)
-
-    output = res.stdout.decode('utf-8') + res.stderr.decode('utf-8')
-
-    if log:
-        print(output)
-    if return_log:
-        return output
-    if err:
-        return res.stderr.decode('utf-8')
     
 def arg_parser() -> dict:
 
